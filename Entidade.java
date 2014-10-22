91f9209c126e39d51077d508b3c00ccb7921e3c8
guiganeto <guiganeto@hotmail.com>
2014-10-22 18:01:07 -0300

    Create Entidade.java

---
+++ Entidade.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bean;
+
+/**
+ *
+ * @author Admin
+ */
+public class Entidade {
+    private String nome_entidade;
+    private String cnpj_entidade;
+
+    /**
+     * @return the nome_entidade
+     */
+    public String getNome_entidade() {
+        return nome_entidade;
+    }
+
+    /**
+     * @param nome_entidade the nome_entidade to set
+     */
+    public void setNome_entidade(String nome_entidade) {
+        this.nome_entidade = nome_entidade;
+    }
+
+    /**
+     * @return the cnpj_entidade
+     */
+    public String getCnpj_entidade() {
+        return cnpj_entidade;
+    }
+
+    /**
+     * @param cnpj_entidade the cnpj_entidade to set
+     */
+    public void setCnpj_entidade(String cnpj_entidade) {
+        this.cnpj_entidade = cnpj_entidade;
+    }
+    
+}
